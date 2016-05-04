302d05f47e50b639bac5a8022de8016f354ed057
Sigalas Alexandros <alxarch@gmail.com>
2016-05-04 17:03:27 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# koa-www-authenticate
+koa-www-authenticate
+====================
+
 Add WWW-Authenticate header on 401 errors
+
+## Usage
+
+```
+wwwAuthenticate = require('koa-www-authenticate');
+app.use(wwwAuthenticate({
+	realm: 'foo',
+	scheme: 'Basic'
+}));
+app.use(function *(next) {
+	this.throw(401, "Test www-authenticate");
+});
+
+// Response will have 'WWW-Authenticate' header set to 'Basic realm="foo"'
+
+```
---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = (options) => {
+	options = Object.assign({
+		scheme: 'Basic',
+		realm: 'Realm'
+	}, options);
+
+	const SCHEME = options.scheme;
+	delete options.scheme;
+
+	const HEADER = Object.keys(options).reduce((result, key) => {
+		result.push(`${key}=\"${result}\"`);
+		return result;
+	}, [SCHEME]).join(' ');
+
+	return function *(next) {
+		try {
+			yield next;
+		}
+		catch (err) {
+			if (401 == err.status) {
+				err.headers = Object.assign({
+					'WWW-Authenticate': HEADER
+				}, err.headers);
+			} 
+			throw err;
+		}
+	};
+};
