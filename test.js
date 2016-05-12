84936cec4896e9687097b1be62abb5b393069cbb
mollyk <katsouli.mo@gmail.com>
2016-05-12 19:47:56 +0300

    Changes in test.js

--- test.js
+++ test.js
@@ -3,28 +3,25 @@
 const assert = require('assert');
 const co = require('co');
 const options = {
-	scheme: null,
-	realm: null
+	scheme: 'Basic',
+	realm: 'fooooo'
 };
 const createWWW = require('./index.js');
 
 describe('tests error throwing', function () {
 	it('tests status error', function () {
+		const ctx = {};
+		const err = new Error();
+		err.status = 401;
+		err.headers = null;
 		let www = createWWW(options);
-		const anError = {
-			status: 401,
-			headers: null
-		};
-		co(www.call(this, Promise.reject(anError)))
-		.catch( function (err) {
-			throw anError;
-		})
-		.then( function (res) {
-			assert.equal(res.status, anError.status, 'Error status is 401');
-			assert.ok(res.headers, "mpla" , 'Error headers are OK');
-			console.log('Hello this is error', err.status, anError.status);
-			console.log('got here');
-			console.log(err.headers);
-		})
+		co(www.call(ctx, Promise.reject(err)))
+			.then(function(res) {
+				assert(false, 'Should never get here');
+			})
+			.catch( function (err) {
+				console.log('Hello this is error', err.headers);
+				assert.equal(err.headers, 'foo', 'Error headers are ok');
+			})
 	});
 });
